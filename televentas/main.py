7c5f04420fa4a9286795c7ac210b5973556c12f8
JuanM <jmalagon72@unisalle.edu.co>
2026-03-19 21:29:52 -0500

    Actualizacion main

--- televentas/main.py
+++ televentas/main.py
@@ -1,13 +1,15 @@
-from models.producto import Producto
+from services.catalogo import Catalogo
 from services.servicio_inventario import ServicioInventario
+from models.producto import Producto
 
 def main():
     inventario = ServicioInventario()
-
     producto = Producto("001", "Laptop", 3000, 10)
+
     inventario.agregar_producto(producto)
 
-    print(inventario.obtener_producto("001"))
+    catalogo = Catalogo(inventario)
+    print(catalogo.consultar_producto("001"))
 
 if __name__ == "__main__":
     main()
